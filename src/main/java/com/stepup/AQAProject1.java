590e340a4354f7288ac17c599637f62faf1cc386
Kirill Gubarev <klg05@mail.ru>
2025-04-17 18:33:15 +0300

    Изменена логика

--- src/main/java/com/stepup/AQAProject1.java
+++ src/main/java/com/stepup/AQAProject1.java
@@ -4,6 +4,12 @@ public class AQAProject1 {
     public static void main(String[] args) {
         System.out.println("Hello_World");
         System.out.println("Hello_My_Friend");
+        System.out.println("Hello_My_Friend2");
+    }
+    public static void doSomething(String[] args) {
+        System.out.println("Hello_World_one_more");
+        System.out.println("Hello_My_Friend_one_more");
+        System.out.println("Hello_My_Friend2_one_more");
     }
 }
 
