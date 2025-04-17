791027226d627e3f835bafdab460aa25d29e2525
Kirill Gubarev <klg05@mail.ru>
2025-04-17 18:42:17 +0300

    Пофиксили баг 143

--- src/main/java/com/stepup/AQAProject1.java
+++ src/main/java/com/stepup/AQAProject1.java
@@ -3,11 +3,12 @@ package com.stepup;
 public class AQAProject1 {
     public static void main(String[] args) {
         System.out.println("Hello_World");
+        System.out.println("Hello_My_Friend2___");
         System.out.println("Hello_My_Friend");
         System.out.println("Hello_My_Friend2");
     }
     public static void doSomething(String[] args) {
-        System.out.println("Hello_World_one_more");
+        System.out.println("Hello_World_one_more_5_times");
         System.out.println("Hello_My_Friend_one_more");
         System.out.println("Hello_My_Friend2_one_more");
     }
