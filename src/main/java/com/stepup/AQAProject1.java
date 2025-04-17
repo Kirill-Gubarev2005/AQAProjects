084cc7a674dde3e7a6c94edf728cc86ba246f3a5
Kirill Gubarev <klg05@mail.ru>
2025-04-17 18:25:40 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/AQAProject1.java
+++ src/main/java/com/stepup/AQAProject1.java
@@ -3,6 +3,7 @@ package com.stepup;
 public class AQAProject1 {
     public static void main(String[] args) {
         System.out.println("Hello_World");
+        System.out.println("Hello_My_Friend");
     }
 }
 
